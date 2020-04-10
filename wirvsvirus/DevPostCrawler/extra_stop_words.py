1bb2caf6e83001c5d14ca972723b8b1162702e33
Guillaume Duc <isotopian@msn.com>
2020-04-10 17:58:34 +0200

    Imporved stop_word list

---
+++ wirvsvirus/DevPostCrawler/extra_stop_words.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+EXTRA_STOP_WORDS = {'able', 'better', 'build', 'built', 'causes', 'challenge',
+                    'challenges', 'changed', 'context', 'corona', 'covid',
+                    'create', 'crisis', 'curious', 'current', 'dear',
+                    'developed', 'different', 'forward', 'got', 'group',
+                    'hackathon', 'hackathons', 'impacts', 'inspiration',
+                    'journey', 'key', 'lot', 'need', 'need', 'new',
+                    'opportunity', 'people', 'presentation', 'problem',
+                    'problems', 'project', 'proud', 'provide', 'ran',
+                    'related', 'risk', 'situations', 'solution', 'solutions',
+                    'start', 'step', 'team', 'time', 'true', 'want', 'want',
+                    'way'}
